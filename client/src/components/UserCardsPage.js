39cda79ec5a6dd9fc2e5cf0c03127559d7ca3b8b
github username <github email address>
2018-03-22 16:10:37 -0500

    fixed issue where user cards show page fails to reender on page refresh

--- client/src/components/UserCardsPage.js
+++ client/src/components/UserCardsPage.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Card from './Card';
 
-class UserCardsPage extends Component {
-	render() {
-		const showCards = this.props.user.cards.map((card) => ( 
+const UserCardsPage = (props) => {
+	let showCards;
+	if (props.user) {
+		showCards = props.user.cards.map((card) => ( 
 			<Card key={card.id} card={card} /> )
 		);
-
-		return (
-			<div>
-				{showCards}
-			</div>
-		);
 	}
+	
+	return (
+		<div>
+			{showCards}
+		</div>
+	);
 };
 
 const mapStateToProps = (state) => ({
