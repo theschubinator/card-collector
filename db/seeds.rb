3bfb745d1a280c8ed44408503a50173bb3cd056c
github username <github email address>
2018-03-30 16:55:09 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,499 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(
+	username: "test", 
+	password: "test", 
+	password_confirmation: "test"
+)
+
+Card.create(
+	brand: "Topps",
+	year: "1984",
+	first_name: "Dan",
+	last_name: "Marino",
+	card_number: "123",
+	value: 5000,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522434273/lo4jsk1sfxvomthnrgtw.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Topps",
+	year: "1981",
+	first_name: "Joe",
+	last_name: "Montana",
+	card_number: "216",
+	value: 10000,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522434536/lb5t7qoaeeoxkhqm7ys8.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Finest",
+	year: "2014",
+	first_name: "Odell",
+	last_name: "Beckham",
+	card_number: "108",
+	value: 300,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522436448/xykgkb7ksuyap9xjwfkr.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Upper Deck",
+	year: "2008",
+	first_name: "Brett",
+	last_name: "Favre",
+	card_number: "275",
+	value: 1000,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522434818/vdoxhgncadvz1nzjpsre.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Stadium Club",
+	year: "1991",
+	first_name: "Brett",
+	last_name: "Favre",
+	card_number: "94",
+	value: 2500,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522434892/scklbup3cty6juoimbxp.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Finest Refractors",
+	year: "1994",
+	first_name: "Brett",
+	last_name: "Favre",
+	card_number: "124",
+	value: 5000,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522434955/owbxsqcabqvzxob7dpsw.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Pacific",
+	year: "1991",
+	first_name: "Emmitt",
+	last_name: "Smith",
+	card_number: "107",
+	value: 250,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522435192/t65fkpsirvly8qhkgfnp.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Pro Set",
+	year: "1990",
+	first_name: "Emmitt",
+	last_name: "Smith",
+	card_number: "685",
+	value: 500,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522435281/xh3gfvnjyutbyhwbjehz.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Topps",
+	year: "1985",
+	first_name: "Dan",
+	last_name: "Marino",
+	card_number: "314",
+	value: 1200,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522435340/q7gcaeb3by0kcp4dxd5l.jpg",
+	orientation: "landscape",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Donruss Elite",
+	year: "2011",
+	first_name: "J.J.",
+	last_name: "Watt",
+	card_number: "145",
+	value: 2000,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522435474/oef7g9lt28xkkipmxd3b.jpg",
+	orientation: "landscape",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Topps Rising Rookies",
+	year: "2011",
+	first_name: "J.J.",
+	last_name: "Watt",
+	card_number: "146",
+	value: 400,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522435597/vvlvbycxcrxtlxmqnkii.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Panini Vertex",
+	year: "2017",
+	first_name: "J.J.",
+	last_name: "Watt",
+	card_number: "14",
+	value: 1500,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522435765/dff92pmpxwkhbo3thswm.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Donruss Gridiron Gear",
+	year: "2005",
+	first_name: "Aaron",
+	last_name: "Rodgers",
+	card_number: "113",
+	value: 8000,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522436044/fildxifdozfwst2kmwl5.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Upper Deck",
+	year: "2008",
+	first_name: "Tom",
+	last_name: "Brady",
+	card_number: "29",
+	value: 250,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522436146/tnhndtdw6f0urerubti9.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Flair Showcase",
+	year: "2014",
+	first_name: "Odell",
+	last_name: "Beckham",
+	card_number: "158",
+	value: 8000,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522436552/na8hhj0jszrpygwk8spq.jpg",
+	orientation: "landscape",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Score",
+	year: "1989",
+	first_name: "Troy",
+	last_name: "Aikman",
+	card_number: "270",
+	value: 2500,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522436697/tcmyuykk7cqswrztfbjq.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Topps",
+	year: "1989",
+	first_name: "Troy",
+	last_name: "Aikman",
+	card_number: "70T",
+	value: 500,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522436734/n6vynvrjdkhk8zdvh5ic.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Upper Deck",
+	year: "1992",
+	first_name: "Troy",
+	last_name: "Aikman",
+	card_number: "G40",
+	value: 75,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522436798/fxfly3vddcb3n6h4gsuj.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Topps",
+	year: "1957",
+	first_name: "Bart",
+	last_name: "Starr",
+	card_number: "119",
+	value: 37500,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522436973/fcttlgkoq824gloculgx.jpg",
+	orientation: "landscape",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Topps",
+	year: "1968",
+	first_name: "Bob",
+	last_name: "Griese",
+	card_number: "196",
+	value: 6000,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522437055/znvjywqljfits5swzl20.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Absolute",
+	year: "2014",
+	first_name: "Jarvis",
+	last_name: "Landry",
+	card_number: "RJJL",
+	value: 1200,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522437161/avcg8cnnov619em5iqxq.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Limited INK",
+	year: "2014",
+	first_name: "Jarvis",
+	last_name: "Landry",
+	card_number: "97",
+	value: 0,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522437271/j3oigg27mbsftni30rhd.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Topps",
+	year: "1989",
+	first_name: "Barry",
+	last_name: "Sanders",
+	card_number: "83T",
+	value: 800,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522437414/c34tjhdgtwzumc3o7ff3.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Upper Deck",
+	year: "2008",
+	first_name: "Barry",
+	last_name: "Sanders",
+	card_number: "278",
+	value: 400,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522437516/phblsnwjjntizcvq6bfu.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Action Packed",
+	year: "1990",
+	first_name: "Barry",
+	last_name: "Sanders",
+	card_number: "78",
+	value: 300,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522437560/z7ipwlden440xrtsdp1p.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Score",
+	year: "1990",
+	first_name: "Barry",
+	last_name: "Sanders",
+	card_number: "3",
+	value: 600,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522437599/oevwuxlnuow7bvuv9yrs.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Topps",
+	year: "1986",
+	first_name: "Jerry ",
+	last_name: "Rice",
+	card_number: "161",
+	value: 5000,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522437786/cc6ir4w5dabgxikd2lug.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "McDonald's",
+	year: "1986",
+	first_name: "Jerry",
+	last_name: "Rice",
+	card_number: "80",
+	value: 400,
+	rookie: false,
+	image_url: "http://res.cloudinary.com/theschubinator/image/upload/v1521934596/c3mqjnvmy4ido233yrht.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Topps",
+	year: "1987",
+	first_name: "Jerry",
+	last_name: "Rice",
+	card_number: "154",
+	value: 60,
+	rookie: false,
+	image_url: "http://res.cloudinary.com/theschubinator/image/upload/v1521934596/c3mqjnvmy4ido233yrht.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Score",
+	year: "1990",
+	first_name: "Jerry",
+	last_name: "Rice",
+	card_number: "100",
+	value: 45,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522437891/wehjyh4qghrulvwiqzrg.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Topps",
+	year: "1990",
+	first_name: "Jerry",
+	last_name: "Rice",
+	card_number: "1",
+	value: 30,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522437928/x247pa0qcrvozmmdnaeo.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Panini Contenders",
+	year: "2012",
+	first_name: "Richard",
+	last_name: "Sherman",
+	card_number: "86A",
+	value: 750,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522438066/yryypc8a3sngrmamwksm.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Bowman",
+	year: "2013",
+	first_name: "Richard",
+	last_name: "Sherman",
+	card_number: "38",
+	value: 23,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522438143/rbgzcmehgxpjo1mfh1w3.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Panini Prizm",
+	year: "2012",
+	first_name: "Cameron",
+	last_name: "Wake",
+	card_number: "103",
+	value: 500,
+	rookie: false,
+	image_url: "http://res.cloudinary.com/theschubinator/image/upload/v1521934596/c3mqjnvmy4ido233yrht.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Finest",
+	year: "2005",
+	first_name: "Dan",
+	last_name: "Marino",
+	card_number: "40",
+	value: 2000,
+	rookie: false,
+	image_url: "http://res.cloudinary.com/theschubinator/image/upload/v1521934596/c3mqjnvmy4ido233yrht.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Philadelphia",
+	year: "1965",
+	first_name: "Paul",
+	last_name: "Warfield",
+	card_number: "41",
+	value: 9000,
+	rookie: true,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522438844/svyzmwlw0qhy7d6z1nbs.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Finest Refractors",
+	year: "1994",
+	first_name: "Dan",
+	last_name: "Marino",
+	card_number: "142",
+	value: 4000,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522438892/oy2iaxz60qoxw1883tva.jpg",
+	orientation: "portrait",
+	user_id: 1
+)
+
+Card.create(
+	brand: "Black Gold",
+	year: "2014",
+	first_name: "Ryan",
+	last_name: "Tannehill",
+	card_number: "SS-RT",
+	value: 3000,
+	rookie: false,
+	image_url: "https://res.cloudinary.com/theschubinator/image/upload/v1522439106/kvtq44qhxaf0u3eqdksi.jpg",
+	orientation: "landscape",
+	user_id: 1
+)
